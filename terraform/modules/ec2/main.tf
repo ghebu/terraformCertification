cb8df4c34c3dc1904f38a6eb92f5677ec307bec0
marius.ghebuta <marius.ghebuta@nagarro.com>
2024-01-10 14:28:12 +0200

    for, for_each, ternary, count

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,11 +1,8 @@
-##first option
-
-
-
+## FOR EACH
 resource "aws_instance" "web" {
-  for_each      = toset(["name1", "name2", "name3"])
-  ami           = "ami-0005e0cfe09cc9050"
-  instance_type = "t3.micro"
+  for_each      = toset(var.instance_names)
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
 
   tags = {
     Name = each.key
@@ -13,3 +10,27 @@ resource "aws_instance" "web" {
 
 }
 
+## FOR can be seen inside outputs.tf
+
+## COUNT and TERNARY OPERATOR
+#COUNT 1st Example
+
+resource "aws_instance" "web1" {
+  count         = 1
+  ami           = var.ami != null ? var.ami : data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+}
+
+
+
+#COUNT 2nd Example
+variable "enabled" {
+  type    = bool
+  default = false
+}
+
+resource "aws_instance" "web2" {
+  count         = var.enabled ? 1 : 0
+  ami           = var.ami
+  instance_type = var.instance_type
+}
--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -3,3 +3,15 @@ variable "instance_names" {
   type        = list(any)
   description = "Var containing EC2 instance names"
 }
+
+variable "instance_type" {
+  default     = "t3.micro"
+  type        = string
+  description = "EC2 instance type"
+}
+
+variable "ami" {
+  default     = "ami-0005e0cfe09cc9050"
+  type        = string
+  description = "The AMI Id to be used for the creation of EC2 instance"
+}
