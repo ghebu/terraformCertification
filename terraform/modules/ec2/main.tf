52a691a87bd01f09d4b7a3636711817967c2cea2
marius.ghebuta <marius.ghebuta@nagarro.com>
2024-01-10 15:32:49 +0200

    locals

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,14 +1,18 @@
 ##first option
 
+locals {
+  tags = {
+    Name      = "Hello World"
+    Terraform = "True"
+  }
+}
 
 
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
 
-  tags = {
-    Name = "Hello world"
-  }
+  tags = local.tags
 
 }
 
